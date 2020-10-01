35e7d724a83b6df2c46e845fd912a4cc8c4f80f3
Oktay Ibis <oktayibis@gmail.com>
2020-10-01 18:58:39 +0300

    head changed

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
+
 import { Main, Footer } from "../../styles/LayoutStyles";
 import { Title } from "../../styles/TextStyles";
 
 export default function Layout({ children }) {
   return (
     <React.Fragment>
+      <Head>
+        <title>Octovan Web Project</title>
+        <meta name="viewport" content="inital-scale=1.0 width=device-width" />
+      </Head>
       <Main>
         <Title>Octavon Web Project</Title>
         {children}
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
-import Document, { Head, Main, NextScript } from "next/document";
-// Import styled components ServerStyleSheet
+import Document, { Head, Main, NextScript, Html } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -17,18 +16,15 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
         <Head>
-          <title>Octavan Web Project</title>
-          {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
-          <meta name="viewport" content="inital-scale=1.0 width=device-width" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
